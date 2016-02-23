aa9716b43b1f0cb53e9e384c28d36adc49aba409
Md Faisal <fsl4faisal@gmail.com>
2016-02-23 16:53:36 +0530

    inserted a record and fetched it in the console

--- src/org/javabrain/faisal/hibernate/HibernateTest.java
+++ src/org/javabrain/faisal/hibernate/HibernateTest.java
@@ -1,5 +1,7 @@
 package org.javabrain.faisal.hibernate;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -8,20 +10,32 @@ import org.javabrains.faisal.dto.UserDetails;
 public class HibernateTest {
 
 	public static void main(String[] args) {
-		UserDetails user=new UserDetails();
-		
+		UserDetails user = new UserDetails();
+
 		user.setUserId(1);
-		user.setUserName("Faisal");
-		
-		SessionFactory sessionFactory= new Configuration().configure().buildSessionFactory();
-		Session session =sessionFactory.openSession();
-		
+		user.setUserName("Raza 3");
+
+		user.setAddress("goplganj");
+		user.setDescription("from Jamia and TCS");
+		user.setJoinDate(new Date());
+
+		// sessionFactory one object per application
+		SessionFactory sessionFactory = new Configuration().configure()
+				.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+
 		session.beginTransaction();
 		session.save(user);
 		session.getTransaction().commit();
-		
-		
+		session.close();
 
+		user = null;
+
+		session = sessionFactory.openSession();
+		session.beginTransaction();
+		user=(UserDetails)session.get(UserDetails.class, 1);
+
+		System.out.println(user.getUserName()+" "+user.getAddress());
 	}
 
 }
--- src/org/javabrains/faisal/dto/UserDetails.java
+++ src/org/javabrains/faisal/dto/UserDetails.java
@@ -1,31 +1,69 @@
 package org.javabrains.faisal.dto;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
+// @Table(name="USER_DETAILS")
 public class UserDetails {
-	
+
 	@Id
 	private int userId;
-	
-	
+
+	public Date getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	private String userName;
-	
-	
+
+	@Temporal(TemporalType.DATE)
+	private Date joinDate;
+
+	private String address;
+
+	// Large OBject lob(byte stream lob) or clob(character lob)
+	@Lob
+	private String description;
+
 	public int getUserId() {
 		return userId;
 	}
+
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	
 
 }
