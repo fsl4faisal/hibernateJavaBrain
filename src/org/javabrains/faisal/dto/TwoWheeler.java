187376131ca961d53fa6557fa91be509d6813da4
Md Faisal <fsl4faisal@gmail.com>
2016-02-26 17:04:44 +0530

    Tutorial 18 -- implemented @DiscrimentorValue and @DiscrimentorColumn --Completed

--- src/org/javabrains/faisal/dto/TwoWheeler.java
+++ src/org/javabrains/faisal/dto/TwoWheeler.java
@@ -1,9 +1,12 @@
 package org.javabrains.faisal.dto;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
 
 @Entity
+//custom D value 
+@DiscriminatorValue("Bike")
 public class TwoWheeler extends Vehicle{
 	private String steeringHandle;
 
--- src/org/javabrains/faisal/dto/Vehicle.java
+++ src/org/javabrains/faisal/dto/Vehicle.java
@@ -1,11 +1,21 @@
 package org.javabrains.faisal.dto;
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 @Entity
 @Table(name="VEHICLE")
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(
+		name="VEHICLE_TYPE",
+		discriminatorType=DiscriminatorType.STRING
+		)
+
 public class Vehicle {
 	@Id
 	@GeneratedValue
