104f6535ea218a6be28b8c855b97b39aea437c12
Md Faisal <fsl4faisal@gmail.com>
2016-02-26 17:57:01 +0530

    Tutorial 21 CRUD--Completed

--- src/org/javabrain/faisal/hibernate/HibernateTest.java
+++ src/org/javabrain/faisal/hibernate/HibernateTest.java
@@ -2,22 +2,12 @@ package org.javabrain.faisal.hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.javabrains.faisal.dto.FourWheeler;
-import org.javabrains.faisal.dto.TwoWheeler;
-import org.javabrains.faisal.dto.Vehicle;
+import org.javabrains.faisal.dto.UserDetails;
 public class HibernateTest {
 
 	public static void main(String[] args) {
-		Vehicle vehicle=new Vehicle();
-		vehicle.setVehicleName("Car");
 		
-		TwoWheeler bike=new TwoWheeler();
-		bike.setVehicleName("Bike");
-		bike.setSteeringHandle("Bike Steering Handle");
 		
-		FourWheeler car=new FourWheeler();
-		car.setVehicleName("Porsche");
-		car.setSteeringWheel("Porsche Steering Wheel");
 		
 		// sessionFactory one object per application
 		SessionFactory sessionFactory = new Configuration().configure()
@@ -26,12 +16,19 @@ public class HibernateTest {
 
 		session.beginTransaction(); 
 		
-		session.save(vehicle);
-		session.save(car);
-		session.save(bike);
-		
+		UserDetails user=(UserDetails)session.get(UserDetails.class, 6);
+		//session.delete(user);
+		user.setUserName("updated user");
+		session.update(user);
+		/*for(int i=0;i<10;i++){
+			UserDetails user=new UserDetails();
+			user.setUserName("User "+i);
+			session.save(user);
+		}*/
 		session.getTransaction().commit();
 		session.close();
+		
+//		System.out.println(user.getUserId()+" "+user.getUserName());
 
 				
 		
--- src/org/javabrains/faisal/dto/UserDetails.java
+++ src/org/javabrains/faisal/dto/UserDetails.java
@@ -1,16 +1,9 @@
 package org.javabrains.faisal.dto;
-import java.util.ArrayList;
-import java.util.Collection;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 @Entity
 @Table(name = "USER_DETAILS")
@@ -23,10 +16,6 @@ public class UserDetails {
 
 	@Column(name = "USER_NAME")
 	private String userName;
-
-	@ManyToMany(cascade=CascadeType.ALL)
-	@JoinTable(name="USER_VEHICLE",joinColumns=@JoinColumn(name="USER_ID"),inverseJoinColumns=@JoinColumn(name="VEHICLE_ID"))
-	private Collection<Vehicle> vehicle = new ArrayList<>();
 
 	public int getUserId() {
 		return userId;
@@ -44,12 +33,5 @@ public class UserDetails {
 		this.userName = userName;
 	}
 
-	public Collection<Vehicle> getVehicle() {
-		return vehicle;
-	}
-
-	public void setVehicle(Collection<Vehicle> vehicle) {
-		this.vehicle = vehicle;
-	}
-
+	
 }
